03e87d2179948475463ecae0e0fbbf600d5bcd34
Rohit Kale <ravneskale@gmail.com>
2023-04-18 21:24:08 -0400

    removing the onclick function

--- src/App.js
+++ src/App.js
@@ -3,15 +3,6 @@ import './App.css';
 import * as d3 from "d3";
 import React, { useState } from 'react';
 
-
-let clicks = 0
-
-function onClick() {
-  clicks += 1;
-  document.getElementById("clicks").innerHTML = clicks;
-}
-
-
 function App() {
   const [click, setClick] = useState(0);
 
